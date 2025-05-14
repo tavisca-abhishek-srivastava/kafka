1035e7c2022404540d71f45b9bb008140b7ca880
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-14 20:59:44 +0530

    first commit

--- youtube/chapter-25/consumer.py
+++ youtube/chapter-25/consumer.py
@@ -1,11 +1,13 @@
 from kafka import KafkaConsumer
 from kafka import TopicPartition , OffsetAndMetadata
 import json
+from random import *
 
 consumer = KafkaConsumer ('hello_world1',bootstrap_servers = ['b-2.tfs3topg.3nd1ah.c1.kafka.us-east-1.amazonaws.com:9092'],
 value_deserializer=lambda m: json.loads(m.decode('utf-8')),group_id='demo112215sgtrjwrykvjh1',auto_offset_reset='earliest', enable_auto_commit =False)
 
 for message in consumer:
+    number = randint(-2147483648 , 2147483647)
     print(message)
     print("The value is : {0}".format(message.value))
     print("The key is : {0}".format(message.key))
@@ -15,6 +17,6 @@ for message in consumer:
     print("The timestamp is : {0}".format(type(message.timestamp)))
     print("The leader_epoch is : {0}".format(message.leader_epoch))
     tp=TopicPartition(message.topic,message.partition)
-    om = OffsetAndMetadata(message.offset + 1, None)
+    om = OffsetAndMetadata(message.offset + 1, message.leader_epoch, number)
     consumer.commit({tp:om})
     print('*' * 100)
