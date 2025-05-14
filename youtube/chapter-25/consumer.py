b8f8ae3695abf6070b0ac8b011d17d4a8965b934
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-14 14:53:27 +0530

    first commit

--- youtube/chapter-25/consumer.py
+++ youtube/chapter-25/consumer.py
@@ -3,16 +3,16 @@ from kafka import TopicPartition , OffsetAndMetadata
 import json
 
 consumer = KafkaConsumer ('hello_world1',bootstrap_servers = ['b-2.tfs3topg.3nd1ah.c1.kafka.us-east-1.amazonaws.com:9092'],
-value_deserializer=lambda m: json.loads(m.decode('utf-8')),group_id='demo112215sgtrjwrykvjh',auto_offset_reset='earliest', enable_auto_commit =False)
+value_deserializer=lambda m: json.loads(m.decode('utf-8')),group_id='demo112215sgtrjwrykvjh1',auto_offset_reset='earliest', enable_auto_commit =False)
 
 for message in consumer:
     print(message)
-    print("The value is : {}".format(message.value))
-    print("The key is : {}".format(message.key))
-    print("The topic is : {}".format(message.topic))
-    print("The partition is : {}".format(message.partition))
-    print("The offset is : {}".format(message.offset))
-    print("The timestamp is : {}".format(message.timestamp))
+    print("The value is : {0}".format(message.value))
+    print("The key is : {0}".format(message.key))
+    print("The topic is : {0}".format(message.topic))
+    print("The partition is : {0}".format(message.partition))
+    print("The offset is : {0}".format(message.offset))
+    print("The timestamp is : {0}".format(message.timestamp))
     tp=TopicPartition(message.topic,message.partition)
     om = OffsetAndMetadata(message.offset+1, message.timestamp)
     consumer.commit({tp:om})
