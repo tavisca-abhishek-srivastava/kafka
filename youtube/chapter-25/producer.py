94eb93e19361ca89451fb5afcebb9ae7e4bc9e72
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-15 08:36:58 +0530

    first commit

--- youtube/chapter-25/producer.py
+++ youtube/chapter-25/producer.py
@@ -13,7 +13,7 @@ producer = KafkaProducer(bootstrap_servers=['b-2.tfs3topg.3nd1ah.c1.kafka.us-eas
 for e in range(100):
     data = {'number' : e}
     key = ["partition-0","partition-1","partition-2"]
-    i = randint(0,1,2)
+    i = randint(0,2)
     print(data)
     print(i)
     producer.send(topic_name, key=(key[i]).encode('utf-8') , value=data)
