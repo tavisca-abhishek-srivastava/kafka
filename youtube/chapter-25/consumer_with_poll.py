7d854da60d050fc82d36cb4724fa85d09a5b4a44
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-15 15:37:18 +0530

    first commit

---
+++ youtube/chapter-25/consumer_with_poll.py
@@ -0,0 +1,13 @@
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer(
+    'my-topic',
+    bootstrap_servers='b-2.tfs3topg.3nd1ah.c1.kafka.us-east-1.amazonaws.com:9092',
+    group_id='my-group',
+    auto_offset_reset='earliest'
+)
+while True:
+    records = consumer.poll(timeout_ms=1000)  # Fetch messages for 1 second
+    for topic_partition, messages in records.items():
+        for message in messages:
+            print(f"{message.topic}:{message.partition} {message.offset}: {message.value}")
