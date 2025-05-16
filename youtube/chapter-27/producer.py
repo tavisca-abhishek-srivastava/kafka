8fe94f5d1001ed17d44ebe668eb5e6a3de2c9b4f
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-16 21:10:34 +0530

    chapter-27

--- youtube/chapter-27/producer.py
+++ youtube/chapter-27/producer.py
@@ -20,7 +20,7 @@ def custom_partitioner(key, all_partitions, available):
 producer = KafkaProducer(bootstrap_servers=['b-2.tfs3topg.3nd1ah.c1.kafka.us-east-1.amazonaws.com:9092'],value_serializer=lambda x: dumps(x).encode('utf-8'),partitioner=custom_partitioner)
 topic_name='consumerlagdemo'
 
-for e in range(0,100):
+for e in range(0,1000):
     data={"number":e}
     producer.send(topic_name, key=str(e).encode(), value=data)
     sleep(.4)
