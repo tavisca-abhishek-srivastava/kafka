e0e0bb1e83603143f96c296b06a4a1bf8cbac59b
Abhishek Srivastava <n769263@cxloyaltycorp.com>
2025-05-18 17:29:07 +0530

    chapter-32

--- youtube/chapter-32/producer.py
+++ youtube/chapter-32/producer.py
@@ -7,17 +7,17 @@ from aws_schema_registry.avro import AvroSchema
 from aws_schema_registry.adapter.kafka import KafkaSerializer
 
 session = boto3.Session(aws_access_key_id='{}', aws_secret_access_key='{}')
-glue_client = session.client('glue', region_name='us-east-1')                      
+glue_client = session.client("glue", region_name="us-east-1")                      
 schema_registry_client = SchemaRegistryClient(glue_client, registry_name='pgsql_nrt_registry')
 serializer = KafkaSerializer(schema_registry_client)
-producer = KafkaProducer(bootstrap_servers=['b-2.tfs3topg.3nd1ah.c1.kafka.us-east-1.amazonaws.com:9092'],value_serializer=serializer)
-with open('./user.avsc', 'r') as schema_file:
-    schema = AvroSchema(schema_file.read())
+#producer = KafkaProducer(bootstrap_servers=["b-2.tfs3topg.3nd1ah.c1.kafka.us-east-1.amazonaws.com:9092"],value_serializer=serializer)
+producer = KafkaProducer(bootstrap_servers=['b-2.tfs3topg.3nd1ah.c1.kafka.us-east-1.amazonaws.com:9092'],value_serializer=lambda x: dumps(x).encode('utf-8')
 data = {
-    'name': 'Hello',
-    'Age':45
-}
-record_metadata =producer.send('glue_schema_bms', value=(data, schema)).get(timeout=10)
+    "name": "Hello",
+    "Age":45 }
+schema_file =  open('./user.avsc', 'r')
+schema = AvroSchema(schema_file.read())
+record_metadata =producer.send("glue_schema_bms", value=(data, schema)).get(timeout=10)
 print(record_metadata.topic)
 print(record_metadata.partition)
 print(record_metadata.offset)
